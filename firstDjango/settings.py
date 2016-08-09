1859dfe12553780fe881cd078298bd700a7a01e3
phapdinh <centralparkloop@yahoo.com>
2016-08-09 02:58:32 -0400

    added templates for views

--- firstDjango/settings.py
+++ firstDjango/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'firstDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['firstDjango/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
