aee145d68e369e0f75d134b8324a24c312858824
phapdinh <centralparkloop@yahoo.com>
2016-08-09 01:55:57 -0400

    added url routes and some views

--- firstDjango/urls.py
+++ firstDjango/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from inventory import views
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'firstDjango.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', views.index, name='index'),
+    url(r'^item/(?P<id>\d+)/', views.item_detail, name='item_detail'),
     url(r'^admin/', include(admin.site.urls)),
 ]
